abd253ae9f2f8bc50dae38a13e6cb8bfa2310f6f
NeithanT <n.vargas.5@estudiantec.cr>
2025-11-01 12:32:42 -0600

    Socket Test With Additional Window

--- GodWars/src/main/java/Server/Server.java
+++ GodWars/src/main/java/Server/Server.java
@@ -1,75 +1,36 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Server;
 
+import Player.Player;
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-/**
- *
- * @author diego
- */
 public class Server {
     private final int PORT = 35500;
-    private final int maxConections = 4;
+    private final int MAX_CONNECTIONS = 4;
+    private ArrayList<Player> players;
     private ServerSocket serverSocket;
-    private ArrayList<ThreadServidor> connectedClients; // arreglo de hilos por cada cliente conectado
-    //referencia a la pantalla
-    FrameServer refFrame;
-    private ThreadConnections connectionsThread;
-
-    public Server(FrameServer refFrame) {
-        connectedClients = new ArrayList<ThreadServidor>();
-        this.refFrame = refFrame;
+    
+    public Server() {
+        System.out.println("Started");
         this.init();
-        connectionsThread = new ThreadConnections(this);
-        connectionsThread.start();
     }
     
-    //método que inicializa el server
-    private void init(){
+    private void init() {
         try {
             serverSocket = new ServerSocket(PORT);
-            refFrame.writeMessage("Server running!!!");
         } catch (IOException ex) {
-            refFrame.writeMessage("Error: " + ex.getMessage());
+            System.out.println("The port is not avalaible");
         }
     }
     
-    public void broadcast(String msg){
-        for (ThreadServidor client : connectedClients) {
-            try {
-                client.sender.writeUTF(msg);
-            } catch (IOException ex) {}
-                
-        }
-
-    }
-
-    public int getMaxConections() {
-        return maxConections;
-    }
-
-    public ServerSocket getServerSocket() {
-        return serverSocket;
-    }
-
-    public ArrayList<ThreadServidor> getConnectedClients() {
-        return connectedClients;
-    }
-
-    public FrameServer getRefFrame() {
-        return refFrame;
+    public void broadcast(String msg) {
+        
     }
     
+    public static void main(String[] args) {
     
-    
+    }
     
     
 
